1c55e36931c17e3d76e0032e3d1b4e6bb842aafd
Toluwalase Adedotun <54019793+toluwalase09@users.noreply.github.com>
2026-09-16 17:28:23 +0000

    final touches

--- main.tf
+++ main.tf
@@ -173,3 +173,59 @@ resource "aws_default_security_group" "database_server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ts_frontend_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend server"
+  }
+}
+
+resource "aws_instance" "ts_backend_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = backend server"
+  }
+}
+
+resource "aws_db_subnet_group" "ts_rds_subnet_group" {
+  name       = "ts_subnet"
+  subnet_ids = [aws_subnet.ts_private_subnet.id, aws_subnet.ts_database_subnet.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "ts_db" {
+  allocated_storage    = 10
+  db_name              = "my_ts_db"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             =  "my-ts-user"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
