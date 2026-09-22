ff330a3d61d89f1b5667c726896b593ece9a1619
Mathew Adelowo <mathewadelowo937@gmail.com>
2026-09-22 16:21:57 +0100

    fixed region

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "ts-academy-state-file"
-    key          = "tf-academy.tfstate"
-    region       = "us-east-1"
+    bucket = "ts-academy-storage"
+    key    = "tf-academy.tfstate"
+    region = var.aws_region
   }
 }
