418a73b613dd7f6ebb8aabe6c24e9dddc5c1fb01
Mathew Adelowo <mathewadelowo937@gmail.com>
2026-09-22 16:37:32 +0100

    output.tf result

--- output.tf
+++ output.tf
@@ -3,17 +3,32 @@ output "availability_zones" {
   value       = data.aws_availability_zones.available.names
 }
 
-output "public_subnet_az" {
-  description = "Availability Zone of the public subnet"
-  value       = aws_subnet.ts_public_subnet.availability_zone
+output "public_subnet_1_az" {
+  description = "Availability Zone of public subnet 1"
+  value       = aws_subnet.ts_public_subnet_1.availability_zone
 }
 
-output "backend_subnet_az" {
-  description = "Availability Zone of the backend subnet"
-  value       = aws_subnet.ts_private_subnet.availability_zone
+output "public_subnet_2_az" {
+  description = "Availability Zone of public subnet 2"
+  value       = aws_subnet.ts_public_subnet_2.availability_zone
 }
 
-output "database_subnet_az" {
-  description = "Availability Zone of the database subnet"
-  value       = aws_subnet.ts_database_subnet.availability_zone
+output "backend_subnet_1_az" {
+  description = "Availability Zone of backend subnet 1"
+  value       = aws_subnet.ts_backend_subnet_1.availability_zone
+}
+
+output "backend_subnet_2_az" {
+  description = "Availability Zone of backend subnet 2"
+  value       = aws_subnet.ts_backend_subnet_2.availability_zone
+}
+
+output "database_subnet_1_az" {
+  description = "Availability Zone of database subnet 1"
+  value       = aws_subnet.ts_database_subnet_1.availability_zone
+}
+
+output "database_subnet_2_az" {
+  description = "Availability Zone of database subnet 2"
+  value       = aws_subnet.ts_database_subnet_2.availability_zone
 }
